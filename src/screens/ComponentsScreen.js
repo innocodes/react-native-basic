c1ee5d84cfab7dab0860143f87478d784ac986f5
innocodes <oyebodeinnocent@gmail.com>
2024-02-13 10:44:48 +0100

    JSX basics

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
  const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>Components Screen</Text>;
+  const greeting = "Hi there";
+  const greetingTwo = <Text style={styles.extraText}>Hello to you.</Text>
+  const name = "Innocent";
+
+  return (
+      <View>
+        <Text style={styles.mainHeaderStyle}>
+          Getting started with react native!
+        </Text>
+        <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        <Text style={styles.textStyle}>{greeting}</Text>
+        {greetingTwo}
+      </View>
+    )
  };
 
  const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
+  },
+  mainHeaderStyle: {
+    fontSize: 45
+  },
+  subHeaderStyle: {
+    fontSize: 20,
+  },
+  extraTextStyle: {
+    fontSize: 20
   }
  });
 
